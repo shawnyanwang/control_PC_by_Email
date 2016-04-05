48360ff431a09ce72c4ffffaa591dc6c50d036e8
shawnyanwang <shawnyanwang@gmail.com>
2016-04-04 18:37:48 -0700

    Change the log string as English

--- utils/excutor.py
+++ utils/excutor.py
@@ -13,28 +13,28 @@ class executor(object):
     def execute(self, exe, mailHelper1):
         self.mailHelper1 = mailHelper1
         subject = exe['subject']
-        self.mccLog.mccWriteLog(u'开始处理命令。')
+        self.mccLog.mccWriteLog('Start to process the command')
         if subject != 'pass':
             self.mailHelper1.sendMail('pass','Slave')
             if subject in self.commandDict:
-                self.mccLog.mccWriteLog(u'执行命令')
+                self.mccLog.mccWriteLog('Run the command')
                 try:
                     command = self.commandDict[subject]
                     os.system(command)
                     self.mailHelper1.sendMail('Success','Boss')
-                    self.mccLog.mccWriteLog(u'执行命令成功')
+                    self.mccLog.mccWriteLog('Successful to run the command')
                 except Exception, e:
-                    self.mccLog.mccError(u'执行命令失败' + str(e))
+                    self.mccLog.mccError('Fail to run the command' + str(e))
                     self.mailHelper1.sendMail('error', 'Boss', e)
             elif subject in self.openDict:
-                self.mccLog.mccWriteLog(u'打开文件')
+                self.mccLog.mccWriteLog('Open the file')
                 try:
                     openFile = self.openDict[subject]
                     win32api.ShellExecute(0, 'open', openFile, '', '', 1)
                     self.mailHelper1.sendMail('Success', 'Boss')
-                    self.mccLog.mccWriteLog(u'打开文件成功')
+                    self.mccLog.mccWriteLog('Successful to open file')
                 except Exception, e:
-                    self.mccLog.mccError(u'打开文件失败：' + str(e))
+                    self.mccLog.mccError('Fail to open file' + str(e))
                     self.mailHelper1.sendMail('error', 'Boss', e)
             elif subject[:7].lower() == 'sandbox':
                 self.sandBox(exe['message'])
